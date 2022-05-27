dbfb23804b75b06d396d758ff6eadc55433e98d3
EastWinds010 <88330283+EastWinds010@users.noreply.github.com>
2022-05-26 21:37:31 -0300

    Update login.dart

--- desafio_login/lib/login.dart
+++ desafio_login/lib/login.dart
@@ -9,20 +9,48 @@ class Login extends StatelessWidget {
       appBar: AppBar(
         title: const Text('RE Database'),
       ),
-      body: Stack(
-        children: [
-          Expanded(child: Container(color: Colors.white,)),
-          Positioned(
-            child: 
-            CircleAvatar(
-              radius:100,
-              backgroundColor: Colors.transparent,
-              child: Image.asset("assets/images/leon-avatar.png")
-            )
-          )
-        ],
-      )
-      
+      body:  Stack(
+        children: [Container(
+          width: MediaQuery.of(context).size.width,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Colors.blue,
+              Colors.lightBlueAccent
+            ])
+          ),
+        ),
+        const Positioned(
+          top: 20,
+          right: 90,
+          child: CircleAvatar(
+            radius:100,
+            backgroundColor: Colors.white,
+            backgroundImage: AssetImage('assets/images/leon-avatar.png'),
+          ),
+        ),
+         const Positioned(
+           width: 360,
+           top: 220,
+           left: 10,
+           child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text('Bem vindo Leon S. Kennedy',
+            style: TextStyle(fontSize: 28.0, color: Colors.white),
+            ),
+                 ),
+         ),
+          const Positioned(
+            top: 300,
+            left: 10,
+            child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text('Leon S. Kennedy. Informações Classificadas.',
+            style: TextStyle(fontSize: 16.0, color: Colors.white),
+            ),
+                  ),
+          ),
+        ] 
+      ),
     );
   }
 }
